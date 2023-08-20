72ab1a23abac9b29ba856b9f2be4037842a7a738
GaliullinAR <galiullin.ar651@gmail.com>
2023-08-20 16:07:53 +0300

    initial project

---
+++ src/components/Overlay.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/Overlay.css';
+
+// const Overlay = () => {
+//   return (
+//     <div className="Overlay"></div>
+//   )
+// }
+
+class Overlay extends React.Component {
+  render() {
+    return <div className='Overlay'></div>
+  }
+}
+
+export default Overlay;
